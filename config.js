b33c46cab38dd194f981d61e8178b06ec12bc067
rkiffer <renankiffer@gmail.com>
2026-07-09 15:09:38 -0400

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Cole aqui a URL do Google Apps Script Web App se quiser salvar online.
-// Exemplo: const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/XXXXX/exec";
-const GOOGLE_SCRIPT_URL = "";
